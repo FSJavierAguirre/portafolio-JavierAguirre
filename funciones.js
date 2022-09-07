9b5c1bd626cd24ab3fc19f9b9640e2eb7cbb09d6
Javier Aguirre <javieraguirreco10@gmail.com>
2022-09-07 15:37:46 -0400

    Se realizan cambios dentro del archivo js

--- funciones.js
+++ funciones.js
@@ -1,4 +1,4 @@
-detalleCotizacion =[]
+detalle = []
 let nombreCliente = prompt('¡Muchísimas gracias por interesarte en mi trabajo como desarrollador! \nPor favor, dime cuál es tu nombre: ')
 while(nombreCliente === ''){
     alert('Por favor ingresa tu nombre')
@@ -12,6 +12,8 @@ while(ingresoCorreo === ''){
 }
 alert('A continuación se te presentarán algunas preguntas. Por favor responde utilizando el número de la opción que desees.')
 let tipoWeb = prompt('Cuéntame, que tipo de página web necesitas cotizar: \n1. Blog \n2. Ecommerce \n3. Landing page')
+
+// Variables
 montoBase = 0
 valorPrimeraOpcion = 0
 valorSegundaOpcion = 0
@@ -28,16 +30,17 @@ while((tipoWeb !== '1') && (tipoWeb !== '2') && (tipoWeb !== '3')){
 if(tipoWeb === '1'){
     montoBase = 150000
     tipoWeb = 'Blog'
-    detalleCotizacion.unshift('Monto base')
-    detalleCotizacion.push(montoBase)
-    primeraOpcionBlog = prompt('¿Sobre qué quieres hablar en tu blog? Elige una opción: \n1. Noticias \n2. Viajes \n3. Recetas \n4. Bitácora \n5. Cine y entretenimiento \n6. Otro')
+    valorInicial = 'Monto base'
+    detalle.unshift(valorInicial)
+    detalle.push(montoBase)
+    let primeraOpcionBlog = prompt('¿Sobre qué quieres hablar en tu blog? Elige una opción: \n1. Noticias \n2. Viajes \n3. Recetas \n4. Bitácora \n5. Cine y entretenimiento \n6. Otro')
     while((primeraOpcionBlog !== '1') && (primeraOpcionBlog !== '2') && (primeraOpcionBlog !== '3') && (primeraOpcionBlog !== '4') && (primeraOpcionBlog !== '5') && (primeraOpcionBlog !== '6')){
         alert('Ingresa una opción válida')
         primeraOpcionBlog = prompt('¿Sobre qué quieres hablar en tu blog? Elige una opción: \n1. Noticias \n2. Viajes \n3. Recetas \n4. Bitácora \n5. Cine y entretenimiento \n6. Otro')
     } if(primeraOpcionBlog === '6'){
         prompt('Cuéntanos sobre qué es tu blog: ')
     }
-    segundaOpcionBlog = prompt('¿Cuantas secciones o categorías tiene tu blog? \n1. De 1 a 5 secciones. \n2. De 6 a 10 secciones. \n3. Más de 10 secciones.')
+    let segundaOpcionBlog = prompt('¿Cuantas secciones o categorías tiene tu blog? \n1. De 1 a 5 secciones. \n2. De 6 a 10 secciones. \n3. Más de 10 secciones.')
     while((segundaOpcionBlog !== '1') && (segundaOpcionBlog !== '2') && (segundaOpcionBlog !== '3')){
         alert('Ingresa una opción válida')
         segundaOpcionBlog = prompt('¿Cuantas secciones o categorías tiene tu blog? \n1. De 1 a 5 secciones. \n2. De 6 a 10 secciones. \n3. Más de 10 secciones.')
@@ -51,9 +54,9 @@ if(tipoWeb === '1'){
         segundaOpcionBlog = 'Más de 10 secciones'
         valorSegundaOpcion = 100000
     }
-    detalleCotizacion.push(segundaOpcionBlog)
-    detalleCotizacion.push(valorSegundaOpcion)
-    terceraOpcionBlog = prompt('¿Cuántas entradas mensuales tendrá tu blog? \n1. De 1 a 10 entradas mensuales. \n2. De 11 a 20 entradas mensuales. \n3. Más de 20 entradas mensuales.')
+    detalle.push(segundaOpcionBlog)
+    detalle.push(valorSegundaOpcion)
+    let terceraOpcionBlog = prompt('¿Cuántas entradas mensuales tendrá tu blog? \n1. De 1 a 10 entradas mensuales. \n2. De 11 a 20 entradas mensuales. \n3. Más de 20 entradas mensuales.')
     while((terceraOpcionBlog !== '1') && (terceraOpcionBlog !== '2') && (terceraOpcionBlog !== '3')){
         alert('Ingresa una opción válida')
         terceraOpcionBlog = prompt('¿Cuántas entradas mensuales tendrá tu blog? \n1. De 1 a 10 entradas mensuales. \n2. De 11 a 20 entradas mensuales. \n3. Más de 20 entradas mensuales.')
@@ -67,9 +70,9 @@ if(tipoWeb === '1'){
         terceraOpcionBlog = 'Más de 20 entradas mensuales'
         valorTerceraOpcion = 100000
     }
-    detalleCotizacion.push(terceraOpcionBlog)
-    detalleCotizacion.push(valorTerceraOpcion)
-    cuartaOpcionBlog = prompt('¿Tu blog contará con un foro? \n1. Sí \n2. No')
+    detalle.push(terceraOpcionBlog)
+    detalle.push(valorTerceraOpcion)
+    let cuartaOpcionBlog = prompt('¿Tu blog contará con un foro? \n1. Sí \n2. No')
     while((cuartaOpcionBlog !== '1') && (cuartaOpcionBlog !== '2')){
         alert('Ingresa una opción correcta')
         cuartaOpcionBlog = prompt('¿Tu blog contará con un foro? \n1. Sí \n2. No')
@@ -80,15 +83,16 @@ if(tipoWeb === '1'){
         cuartaOpcionBlog = 'El blog no contará con un foro'
         valorCuartaOpcion = 0
     }
-    detalleCotizacion.push(cuartaOpcionBlog)
-    detalleCotizacion.push(valorCuartaOpcion)
+    detalle.push(cuartaOpcionBlog)
+    detalle.push(valorCuartaOpcion)
 }
 
 else if(tipoWeb === '2'){
     montoBase = 250000
     tipoWeb = 'Ecommerce'
-    detalleCotizacion.unshift('Monto base')
-    detalleCotizacion.unshift(montoBase)
+    valorInicial = 'Monto base'
+    detalle.unshift(valorInicial)
+    detalle.push(montoBase)
     primeraOpcionEcommerce = prompt('¿Sobre qué es tu marca? \n1. Tecnología \n2. Servicios \n4. Alimentos \n5. Deco \n6. Juguetes \n7. Otro')
     while((primeraOpcionEcommerce !== '1') && (primeraOpcionEcommerce !== '2') && (primeraOpcionEcommerce !== '3') && (primeraOpcionEcommerce !== '4') && (primeraOpcionEcommerce !== '5') && (primeraOpcionEcommerce !== '6') && (primeraOpcionEcommerce !== '7')){
         alert('Ingrese una opción correcta')
@@ -96,86 +100,105 @@ else if(tipoWeb === '2'){
     } if(primeraOpcionEcommerce === '7'){
         prompt('Ingresa sobre qué es tu marca')
     }
-    segundaOpcionEcommerce = prompt('¿Cuantos productos manejas? \n1. De 1 a 20 productos. \n2. De 21 a 50 productos. \n3. Más de 50 productos.')
+    let segundaOpcionEcommerce = prompt('¿Cuantos productos manejas? \n1. De 1 a 20 productos. \n2. De 21 a 50 productos. \n3. Más de 50 productos.')
     while((segundaOpcionEcommerce !== '1') && (segundaOpcionEcommerce !== '2') && (segundaOpcionEcommerce !== '3')){
         alert('Ingrese una opción correcta')
         segundaOpcionEcommerce = prompt('¿Cuantos productos manejas? \n1. De 1 a 20 productos. \n2. De 21 a 50 productos. \n3. Más de 50 productos.')
     } if(segundaOpcionEcommerce === '1'){
+        segundaOpcionEcommerce = 'De 1 a 20 productos'
         valorSegundaOpcion = 0
     } else if(segundaOpcionEcommerce === '2'){
+        segundaOpcionEcommerce = 'De 20 a 50 productos'
         valorSegundaOpcion = 100000
     } else if(segundaOpcionEcommerce === '3'){
+        segundaOpcionEcommerce = 'Más de 50 productos'
         valorSegundaOpcion = 150000
     }
-    detalleCotizacion.push(valorSegundaOpcion)
-    terceraOpcionEcommerce = prompt('¿Cuentas con fotografías profesionales de tus productos? \n1. Sí \n2. No')
+    detalle.push(segundaOpcionEcommerce)
+    detalle.push(valorSegundaOpcion)
+    let terceraOpcionEcommerce = prompt('¿Cuentas con fotografías profesionales de tus productos? \n1. Sí \n2. No')
     while((terceraOpcionEcommerce !== '1') && (terceraOpcionEcommerce !== '2')){
         alert('Ingrese una opción correcta')
         terceraOpcionEcommerce = prompt('¿Cuentas con fotografías profesionales de tus productos? \n1. Sí \n2. No')
     } if(terceraOpcionEcommerce === '1'){
+        terceraOpcionEcommerce = 'Cuento con fotografías profesionales de los productos'
         valorTerceraOpcion
     } else if(terceraOpcionEcommerce === '2'){
+        terceraOpcionEcommerce = 'Necesito fotografías profesionales de los productos'
         valorTerceraOpcion = 100000
     }
-    detalleCotizacion.push(valorTerceraOpcion)
-    cuartaOpcionEcommerce = prompt('¿Tus clientes necesitan loguearse para comprar? \n1. Sí \n2. No')
+    detalle.push(terceraOpcionEcommerce)
+    detalle.push(terceraSegundaOpcion)
+    let cuartaOpcionEcommerce = prompt('¿Tus clientes necesitan loguearse para comprar? \n1. Sí \n2. No')
     while((cuartaOpcionEcommerce !== '1') && (cuartaOpcionEcommerce !== '2')){
         alert('Ingrese una opción correcta')
         cuartaOpcionEcommerce = prompt('¿Tus clientes necesitan loguearse para comprar? \n1. Sí \n2. No')
     } if(cuartaOpcionEcommerce === '1'){
+        cuartaOpcionEcommerce = 'Mis clientes necesitan loguearse para comprar'
         valorCuartaOpcion = 50000
     } else if(cuartaOpcionEcommerce === '2'){
+        cuartaOpcionEcommerce = 'Mis clientes no necesitan loguearse para comprar'
         valorCuartaOpcion
     }
-    detalleCotizacion.push(valorCuartaOpcion)
-    quintaOpcionEcommerce = prompt('¿Tus clientes necesitarán un asistente virtual para realizar sus compras? \n1. Sí \n2. No')
+    detalle.push(cuartaOpcionEcommerce)
+    detalle.push(valorCuartaOpcion)
+    let quintaOpcionEcommerce = prompt('¿Tus clientes necesitarán un asistente virtual para realizar sus compras? \n1. Sí \n2. No')
     while((quintaOpcionEcommerce !== '1') && (quintaOpcionEcommerce !== '2')){
         alert('Ingrese una opción correcta')
         quintaOpcionEcommerce = prompt('¿Tus clientes necesitarán un asistente virtual para realizar sus compras? \n1. Sí \n2. No')
     } if(quintaOpcionEcommerce === '1'){
+        quintaOpcionEcommerce = 'Mis clientes necesitarán asistente virtual para realizar sus compras'
         valorQuintaOpcion = 50000
     } else if(quintaOpcionEcommerce === '2'){
+        quintaOpcionEcommerce = 'Mis clientes no necesitarán asistente virtual para realizar sus compras'
         valorQuintaOpcion
     }
-    detalleCotizacion.push(valorQuintaOpcion)
-    sextaOpcionEcommerce = prompt('¿Tienes varias categorías de productos? \n1. Sí \n2. No')
+    detalle.push(quintaOpcionEcommerce)
+    detalle.push(valorQuintaOpcion)
+    let sextaOpcionEcommerce = prompt('¿Tienes varias categorías de productos? \n1. Sí \n2. No')
     while((sextaOpcionEcommerce !== '1') && (sextaOpcionEcommerce !== '2')){
         alert('Ingrese una opción correcta')
         sextaOpcionEcommerce = prompt('¿Tienes varias categorías de productos? \n1. Sí \n2. No')
     }
-    detalleCotizacion.push(valorSextaOpcion)
 }
 
 else if(tipoWeb === '3'){
     montoBase = 150000
     tipoWeb = 'Landing page'
-    detalleCotizacion.unshift('Monto base')
-    detalleCotizacion.unshift(montoBase)
+    valorInicial = 'Monto base'
+    detalle.unshift(valorInicial)
+    detalle.push(montoBase)
     primeraOpcionLanding = prompt('¿Que quieres promocionar en tu landing page? \n1. Un producto \n2. Un servicio \n3. Una marca \n4. Una ONG')
     while((primeraOpcionLanding !== '1') && (primeraOpcionLanding !== '2') && (primeraOpcionLanding !== '3') && (primeraOpcionLanding !== '4')){
         alert('Ingrese una opción corecta')
         primeraOpcionLanding = prompt('¿Que quieres promocionar en tu landing page? \n1. Un producto \n2. Un servicio \n3. Una marca \n4. Una ONG')
     }
-    segundaOpcionLanding = prompt('¿Necesitas captar datos de los visitantes? \n1. Sí \n2. No')
+    let segundaOpcionLanding = prompt('¿Necesitas captar datos de los visitantes? \n1. Sí \n2. No')
     while((segundaOpcionLanding !== '1') && (segundaOpcionLanding !== '2')){
         alert('Ingrese una opción correcta')
         segundaOpcionLanding = prompt('¿Necesitas captar datos de los visitantes? \n1. Sí \n2. No')
     } if(segundaOpcionLanding === '1'){
+        segundaOpcionLanding = 'Necesitas captar datos de visitantes'
         valorSegundaOpcion = 100000
     } else{
+        segundaOpcionLanding = 'No necesitas captar datos de visitantes'
         valorSegundaOpcion
     }
-    detalleCotizacion.push(valorSegundaOpcion)
-    terceraOpcionLanding = prompt('¿Necesitas enviarle algún regalo a los visitantes? \n1. Sí \n2. No')
+    detalle.push(segundaOpcionLanding)
+    detalle.push(valorSegundaOpcion)
+    let terceraOpcionLanding = prompt('¿Necesitas enviarle algún regalo a los visitantes? \n1. Sí \n2. No')
     while((terceraOpcionLanding !== '1') && (terceraOpcionLanding !== '2')){
         alert('Ingrese una opción correcta')
         terceraOpcionLanding = prompt('¿Necesitas enviarle algún regalo a los visitantes? \n1. Sí \n2. No')
     } if(terceraOpcionLanding === '1'){
+        terceraOpcionLanding = 'Necesitas enviarle regalos promocionales a los visitantes'
         valorTerceraOpcion = 50000
     } else{
+        terceraOpcionLanding = 'No necesitas enviarle regalos promocionales a los visitantes'
         valorTerceraOpcion
     }
-    detalleCotizacion.push(valorTerceraOpcion)
+    detalle.push(terceraOpcionLanding)
+    detalle.push(valorTerceraOpcion)
 }
 
 function sumar(){
@@ -200,11 +223,14 @@ alert('La cotización de tu '+tipoWeb+ ' es de '+pesosChilenos.format(valorFinal
 alert('Muchas gracias por cotizar conmigo ' +nombreCliente +'. Tu cotización será enviada a '+ingresoCorreo)
 alert('¡Cuídate!')
 
+// Objetos
+
 let tarjetaCliente = {
     nombre: nombreCliente,
     email: ingresoCorreo,
     servicioSeleccionado: tipoWeb,
     valorTotal: pesosChilenos.format(valorFinal),
-    detalle: detalleCotizacion
+    detalle: detalle
 }
+
 console.log(tarjetaCliente)
